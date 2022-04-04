286f7fd710140354f07490a663ad4da789d7ffc5
kojiman-git <kojitatu3@gmail.com>
2022-04-04 22:33:28 +0900

    トップページレイアウト改善

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,6 @@ Rails.application.routes.draw do
   get 'home_page/news'
   get 'home_page/direct_message'
   get 'home_page/post'
-  get 'static_pages/app_explanation'
   root 'static_pages#top'
 
   get    '/login',   to: 'sessions#new'
