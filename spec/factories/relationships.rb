c998c5ef3e4c6b3b03bbbdec47e6e23632b3b858
kojiman-git <kojitatu3@gmail.com>
2022-04-22 01:37:55 +0900

    デリートリクエストのテスト完了

--- spec/factories/relationships.rb
+++ spec/factories/relationships.rb
@@ -4,5 +4,13 @@ FactoryBot.define do
       follower_id {"1"}
       followed_id { "2" } 
     end
+    trait :b do
+      follower_id {"1"}
+      followed_id { "3" } 
+    end
+    trait :c do
+      follower_id {"3"}
+      followed_id { "1" } 
+    end
   end
 end
---
+++ spec/requests/delete_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe "delete", type: :request do
+  describe "[Action test]" do
+
+    before do
+      @user1 = FactoryBot.create(:user,:a)
+      @user2 = FactoryBot.create(:user,:b)
+      @user3 = FactoryBot.create(:user,:c)
+      post login_path, 
+      params: { session: { email: @user1.email,
+        password: @user1.password } }
+      @category = FactoryBot.create(:m_category, :a)
+      @category = FactoryBot.create(:m_category, :b)
+      @category = FactoryBot.create(:m_category, :c)
+      @post1 = FactoryBot.create(:post,:a)
+      @post2 = FactoryBot.create(:post,:b)
+      @post3 = FactoryBot.create(:post,:c)
+      @like = FactoryBot.create(:like,:a)
+      @room = FactoryBot.create(:room,:a)
+      @entry1 = FactoryBot.create(:entry,:a)
+      @entry2 = FactoryBot.create(:entry,:b)
+      @relationship = FactoryBot.create(:relationship,:a)
+    end
+
+    it "投稿を削除できること" do
+      delete "/posts/#{@post1.id}"
+      expect(Post.count).to eq 2
+    end
+    it "フォローを解除できること" do
+      delete "/relationships/1"
+      expect(Relationship.count).to eq 0
+    end
+
+    
+  end
+end
