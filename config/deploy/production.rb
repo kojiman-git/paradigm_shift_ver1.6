142865142872faf646b7bc19c4f4085b0588ae8a
kojiman-git <kojitatu3@gmail.com>
2022-07-10 21:27:02 +0900

    EC2にCDできるように10

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -62,6 +62,8 @@
 server '35.77.138.122', user: 'ec2-user', roles: %w{app db web}
 
 set :ssh_options, {
+  
+  user: "ec2-user",
   keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
   forward_agent: true,
   auth_methods: %w[publickey]
