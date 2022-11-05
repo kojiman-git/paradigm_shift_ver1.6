97a4fef4a6bea2911378b1533af516ce6a28efbc
Mitsuru53 <exhikkii@icloud.com>
2022-11-05 16:44:07 +0900

    user_specにAAAパターンを意識したサンプルコードを追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe "#validations" do
+    # テストケースごとに変わる値部分はletで定義し、テストケースごとに上書きできるようにしておく
+    let(:user) { FactoryBot.build(name: name, email: email, password_digest: password_digest) }
+    let(:name) { "user name" }
+    let(:email) { "tester@example.com" }
+    let(:password_digest) { "dottle-nouveau-pavilion-tights-furze" }
+
+    context "メールアドレスがない場合" do
+      let(:email) { nil }
+
+      it "バリデーションエラーとなる" do
+        user.valid?
+        
+        expect(user.errors[:email]).to include("can't be blank")
+      end
+    end
+
+    context "メールアドレスが既に登録済みの場合" do
+      # AAAパターンを意識する
+      # 準備(arrange)
+      before do
+        FactoryBot.create(name: "another user", email: email, password_digest: "dottle-nouveau-pavilion-tights-furze")
+      end
+
+      it "バリデーションエラーとなる" do
+        # 実行(act)
+        user.valid?
+        
+        # 検証(assert)
+        expect(user.errors[:email]).to include("has already been taken")
+      end
+    end
+  end
+
   # 名、メール、パスワードがあれば有効な状態であること
   it "is valid with a first name, last name, email, and password" do
     user =  FactoryBot.create(:user,:a)
