6124e6d7dc3511b93ed36495c51c5ef6a1ae6517
kojiman-git <kojitatu3@gmail.com>
2023-01-05 04:50:52 +0900

    投稿検索機能の実装が完了

--- back-end/app/controllers/search_controller.rb
+++ back-end/app/controllers/search_controller.rb
@@ -1,16 +1,31 @@
 class SearchController < ApplicationController
   def post_search
     @posts = Post.all
-    @post = Post.new
+
+    postList = []
+
+    @posts.each do |post|
+      postList.push({post_id:post.id,user_id:post.user.id,user_name:post.user.name,term:post.term,paraphrase:post.paraphrase,category:post.m_category.name,created_at: post.created_at,avg_score:post.avg_score,reviewsCount:post.reviews.count,userImage:post.user.image.thumb.url,Liked:post.liked_by?(current_user),sameID:sameID?(post)})
+    end
+
+    render json: postList
   end
   
   def narrow_down_by_category
-    @posts = Post.where(category_params)
-    
-    if @posts[0].nil?
+    @posts = Post.where(params.require(:post).permit(:m_category_id))
+  
+    if params[:post][:m_category_id] == "7"
       @posts = Post.all
     end
-    @post = Post.new
+
+    postList = []
+
+    @posts.each do |post|
+      postList.push({post_id:post.id,user_id:post.user.id,user_name:post.user.name,term:post.term,paraphrase:post.paraphrase,category:post.m_category.name,created_at: post.created_at,avg_score:post.avg_score,reviewsCount:post.reviews.count,userImage:post.user.image.thumb.url,Liked:post.liked_by?(current_user),sameID:sameID?(post)})
+    end
+
+    render json: postList
+    
   end
 
   def user_search
@@ -32,6 +47,12 @@ class SearchController < ApplicationController
       params.require(:post).permit(:m_category_id)
     end
 
-  
+    def sameID?(post)
+      if post.user.id == current_user.id
+        true
+      else
+        false
+      end
+    end
 
 end
