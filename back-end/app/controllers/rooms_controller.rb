7d1a1325fb8b891f194b8a1cefe24fce4f55d564
kojiman-git <kojitatu3@gmail.com>
2023-01-10 07:33:11 +0900

    ダイレクトメッセージ機能の実装が完了

--- back-end/app/controllers/rooms_controller.rb
+++ back-end/app/controllers/rooms_controller.rb
@@ -4,8 +4,9 @@ class RoomsController < ApplicationController
     @room = Room.create(user_id: current_user.id)
     @entry1 = Entry.create(room_id: @room.id, user_id: current_user.id)
     @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id))
-    redirect_to "/rooms/#{@room.id}"
     
+    payload = { RoomId: @room.id }
+    render json: payload
   end
 
   def show
