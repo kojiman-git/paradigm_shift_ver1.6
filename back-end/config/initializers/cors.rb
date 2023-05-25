689a67f93ab521fcac4a9bacc18e45a60f504c0d
kojiman-git <kojitatu3@gmail.com>
2023-05-26 05:09:29 +0900

    本番環境用データベース情報を環境変数に変更

--- back-end/config/initializers/cors.rb
+++ back-end/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://spa-front-paradigm-shift.work/'
+    origins 'https://spa-front-paradigm-shift.work'
 
     resource '*',
       headers: :any,
