6d89be03bed32d6eda1dc363f32b0cb4208e379a
kojiman-git <kojitatu3@gmail.com>
2023-07-18 00:06:43 +0900

    アイコンにコメントを追加、トップ画面に具体的な使用方法を追加、Descriptionの解説内容を変更

--- front-end/components/atoms/AppExplanation.vue
+++ front-end/components/atoms/AppExplanation.vue
@@ -1,7 +1,10 @@
 <template>
   <div class = up>
-        <p id = "app-explanation">このアプリはアクティブラーニングで重要な要素である再言語化</p>
-        <p id = "app-explanation">と想起を元にした学習を支援するSNSサービスになります</p>
+        <p id = "app-explanation">本アプリは、あなたの学習を効果的にサポートするためのSNSサービスです</p>
+        <p id = "app-explanation">本アプリの具体的な使用方法としては単語帳をイメージしてください</p>
+        <p id = "app-explanation">あなたが学習したい分野の単語を自分自身の言葉で表現してそれを投稿します</p>
+        <p id = "app-explanation">投稿をお気に入りに追加することでクイズからその内容を出題することができます</p>
+        <p id = "app-explanation">オリジナルな単語帳を作成し、より深い理解と記憶の定着を達成しましょう</p>
   </div>
 </template>
 
@@ -18,7 +21,7 @@ export default {
 }
 
 #app-explanation{
-  font-size: 3vw;
+  font-size: 2.2vw;
 }
 
 </style>
